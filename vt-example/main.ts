c9a89c449e28ef8995b6537ccc0f85c2cf00c63a
puffyCid <16283453+puffyCid@users.noreply.github.com>
2023-11-07 22:39:46 -0500

    vt example

--- vt-example/main.ts
+++ vt-example/main.ts
@@ -1,13 +1,15 @@
 import { processListing } from "https://raw.githubusercontent.com/puffycid/artemis-api/master/src/system/memory.ts";
-import { VirusTotal } from "../../artemis-api/src/http/vt.ts";
-import { VTFile } from "../../artemis-api/types/http/vt.ts";
-
+import { VirusTotal } from "https://raw.githubusercontent.com/puffycid/artemis-api/master/src/http/vt.ts";
+import {
+  VTData,
+  VTFile,
+} from "https://raw.githubusercontent.com/puffycid/artemis-api/master/types/http/vt.ts";
 interface ProcVT {
   proc: unknown;
   vt: VTFile;
 }
 async function main() {
-  const key = "<YOUR KEY HERE!!>";
+  const key = "<YOUR API KEY HERE>";
   const vtClient = new VirusTotal(key);
 
   const processes = processListing(true);
@@ -16,26 +18,34 @@ async function main() {
 
   const vtProc = [];
   for (const proc of processes) {
-    count += 1;
+    // Public API limited to 4 lookups per min :(
+    if (count >= publicLimit) {
+      break;
+    }
+
     const md5 = proc.md5;
     const vtResponse = await vtClient.lookupHash(md5);
     if (vtResponse instanceof Error) {
       continue;
     }
 
+    count += 1;
+
+    if (vtResponse.status != 200) {
+      continue;
+    }
+
+    const file = vtResponse.body as VTData;
+
     const procVT: ProcVT = {
       proc,
-      vt: vtResponse.body as VTFile,
+      vt: file.data.attributes as VTFile,
     };
 
     vtProc.push(procVT);
-    // Public API limited to 4 lookups per min :(
-    if (count >= publicLimit) {
-      break;
-    }
   }
 
-  console.log(vtProc);
+  console.log(vtProc[0]);
 }
 
 main();
